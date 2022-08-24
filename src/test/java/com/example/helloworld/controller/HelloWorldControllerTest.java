66baeb94b02772eb22f7e6b919a50fbb0b4fca00
whitespacemx <sistemas@whitespace.com.mx>
2022-08-24 00:08:54 -0500

    Modificacion de test

--- src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
@@ -22,6 +22,6 @@ public class HelloWorldControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello, World!"));
+                .andExpect(content().string("Hola, BLUE!"));
     }
 }
